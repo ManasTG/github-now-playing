6a512b56499f2edd323554a049629ed3eac11769
Manas Madhukar <manas.jilal@gmail.com>
2026-07-12 16:52:39 +0530

    caching for lastfm.js

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
 import http from "node:http";
-import "dotenv/config";
+import { getCurrentTrack } from "./lastfm.js";
 import { generateSvg } from "./svg.js";
-
-const API_KEY = process.env.LASTFM_API_KEY;
-const USER = process.env.LASTFM_USER;
 
 http.createServer(async (req, res) => {
 
-    const url =
-    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USER}&api_key=${API_KEY}&format=json&limit=1`;
+    const track = await getCurrentTrack();
 
-    const response = await fetch(url);
-    const data = await response.json();
-
-    //console.log(JSON.stringify(data, null, 2));
-
-    const track = data.recenttracks.track[0];
-
-    const title = track.name;
-    const artist = track.artist["#text"];
-    const album = track.album["#text"];
-    const trackUrl = track.url;
-    const playedAt = track.date?.["#text"] ?? "Now Playing";
-
-    const isPlaying = track["@attr"]?.nowplaying === "true";
-
-    console.log(title);
-    console.log(artist);
-    console.log(album);
-    console.log(playedAt);
-
-    const svg = generateSvg({
-        title,
-        artist,
-        isPlaying,
-    });
+    const svg = generateSvg(track);
 
     res.writeHead(200, {
         "Content-Type": "image/svg+xml",
     });
 
     res.end(svg);
-}).listen(3001 , () => {
+
+}).listen(3001, () => {
     console.log("🚀 Server running at http://localhost:3001");
 });
--- lastfm.js
+++ lastfm.js
@@ -1,21 +1,62 @@
-// lastfm.js
 import "dotenv/config";
 
 const API_KEY = process.env.LASTFM_API_KEY;
 const USER = process.env.LASTFM_USER;
 
+// Cache
+let cachedTrack = null;
+let cacheTime = 0;
+
+const CACHE_DURATION = 30 * 1000;
+
 export async function getCurrentTrack() {
-    const apiUrl =
-    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USER}&api_key=${API_KEY}&format=json&limit=1`;
 
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+    // Cache hit
+    if (
+        cachedTrack &&
+        Date.now() - cacheTime < CACHE_DURATION
+    ) {
+        console.log("Using cache");
+        return cachedTrack;
+    }
 
-    const track = data.recenttracks.track[0];
+    console.log("Fetching from Last.fm");
 
-    return {
-        title: track.name,
-        artist: track.artist["#text"],
-        isPlaying: track["@attr"]?.nowplaying === "true",
-    };
+    try {
+
+        const apiUrl =
+        `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USER}&api_key=${API_KEY}&format=json&limit=1`;
+
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error(`Last.fm responded with ${response.status}`);
+        }
+
+        const data = await response.json();
+        const track = data.recenttracks.track[0];
+
+        const currentTrack = {
+            title: track.name,
+            artist: track.artist["#text"],
+            album: track.album["#text"],
+            isPlaying: track["@attr"]?.nowplaying === "true",
+        };
+
+        cachedTrack = currentTrack;
+        cacheTime = Date.now();
+
+        return currentTrack;
+
+    } catch (err) {
+
+        console.error(err);
+
+        if (cachedTrack) {
+            console.log("Returning cached track");
+            return cachedTrack;
+        }
+
+        throw err;
+    }
 }
