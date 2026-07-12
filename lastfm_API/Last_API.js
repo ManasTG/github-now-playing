0acbd367ae4028df23d9359027f24870b9abe05b
Manas Madhukar <manas.jilal@gmail.com>
2026-07-12 16:05:32 +0530

    redo

--- lastfm_API/Last_API.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = "8b3e8b2e0667a9c97ad960381d0a40a5";
-const USER = "ManasTG";
-
-const url =
-`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USER}&api_key=${API_KEY}&format=json&limit=1`;
